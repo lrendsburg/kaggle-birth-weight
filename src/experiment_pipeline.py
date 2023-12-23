cc9cbf01c269e4f22abd80a4592fbab09868f476
Luca Rendsburg <luca.rendsburg@gmail.com>
2023-12-23 09:48:52 +0100

    feat(main): add basic experiment structure

--- src/experiment_pipeline.py
+++ src/experiment_pipeline.py
@@ -1,7 +1,19 @@
 from abc import ABC, abstractmethod
+import os
+from pathlib import Path
+import logging
+
+import numpy as np
+import pandas as pd
+import joblib
+
+from src.utils.metrics import Metrics
+from src.utils.logging import log_results
+from src.utils.analysis import Analysis
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
 
 
-# TODO: implement
 class BaseExperiment(ABC):
     """
     Base class for model benchmarking experiments.
@@ -28,19 +40,88 @@ class BaseExperiment(ABC):
         model.run_experiment()
     """
 
-    def __init__(self):
-        pass
+    def __init__(
+        self, dataset: str, make_analysis: bool = False, make_prediction: bool = False
+    ):
+        self.dataset = dataset
+        self._validate_dataset()
 
-    def _load_data(self):
-        pass
+        self.make_analysis = make_analysis
+        self.make_prediction = make_prediction
+
+        self.model_name = self.__class__.__name__
+
+    def _validate_dataset(self):
+        valid_datasets = [f.name for f in Path("datasets").iterdir() if f.is_dir()]
+        if self.dataset not in valid_datasets:
+            raise ValueError(
+                f"Invalid dataset {self.dataset}. Valid datasets are {valid_datasets}"
+            )
+
+    def _load_dataset(self):
+        dataset_path = Path("datasets", self.dataset)
+
+        X_train = np.load(Path(dataset_path, "X_train.npy"))
+        y_train = np.load(Path(dataset_path, "y_train.npy"))
+        X_val = np.load(Path(dataset_path, "X_val.npy"))
+        y_val = np.load(Path(dataset_path, "y_val.npy"))
+        X_test = np.load(Path(dataset_path, "X_test.npy"))
+        try:
+            target_transform = joblib.load(Path(dataset_path, "target_transform.save"))
+        except FileNotFoundError:
+            target_transform = None
+
+        return X_train, y_train, X_val, y_val, X_test, target_transform
+
+    def _predict_test(self, X_test: np.ndarray, target_transform) -> None:
+        """Save predictions on test set."""
+        y_pred = self.predict(X_test)
+        # Return to original scale
+        if target_transform is not None:
+            y_pred = target_transform.inverse_transform(y_pred)
+
+        df = pd.read_csv(Path("predictions", "sample_submission.csv"))
+        df[["pi_lower", "pi_upper"]] = y_pred
+        df.to_csv(Path("predictions", f"{self.model_name}.csv"), index=False)
 
     def run_experiment(self):
+        X_train, y_train, X_val, y_val, X_test, target_transform = self._load_dataset()
+
+        logging.info(f"Fitting model '{self.model_name}' on dataset '{self.dataset}'.")
+        self.fit(X_train, y_train, X_val, y_val)
+
+        for X, y, stage in [(X_train, y_train, "train"), (X_val, y_val, "val")]:
+            y_pred = self.predict(X)
+
+            logging.info(f"({stage}): computing and logging metrics.")
+            metrics = Metrics(y, y_pred).compute_metrics()
+            log_results(metrics, stage)
+
+            if self.make_analysis:
+                logging.info(f"({stage}): running analysis.")
+                analysis = Analysis(
+                    y,
+                    y_pred,
+                    stage,
+                    model_name=self.model_name,
+                    save_path=Path("results"),
+                )
+                analysis.full_analysis()
+
+        if self.make_prediction:
+            logging.info("({stage}): making predictions.")
+            self._predict_test(X_test, target_transform)
+
+    @abstractmethod
+    def fit(
+        self,
+        X_train: np.ndarray,
+        y_train: np.ndarray,
+        X_val: np.ndarray,
+        y_val: np.ndarray,
+    ) -> None:
         pass
 
     @abstractmethod
-    def fit(self):
-        pass
-
-    @abstractmethod
-    def predict(self):
+    def predict(self, X: np.ndarray) -> np.ndarray:
         pass
