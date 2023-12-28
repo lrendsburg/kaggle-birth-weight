ecb19cf2a47f0b89df85035cb189be5c24c7f8a1
Luca Rendsburg <luca.rendsburg@gmail.com>
2023-12-28 19:21:29 +0100

    feat(prediction): add Bayesian linear regression prediction head

--- src/utils/prediction.py
+++ src/utils/prediction.py
@@ -4,6 +4,8 @@ from abc import ABC, abstractmethod
 import numpy as np
 from numpy import ndarray
 
+from src.utils.bayesian_linear_regression import BayesianLinearRegression
+
 
 class Prediction(ABC):
     """Generic base class that converts any type of model output to a specific prediction (confidence intervals)."""
@@ -148,4 +150,87 @@ class ConformalPrediction(Prediction):
         return {
             "prediction_type": "conformal",
             "conformal_coverage": self.coverage,
+        }
+
+
+class BayesianPrediction(Prediction):
+    """Predicts confidence intervals based on Bayesian linear regression.
+
+    The forward method is assumed to simply pre-process the data to some other
+    feature space R^d -> R^d'. A Bayesian linear regression model is then fitted on
+    the pre-processed data. Confidence intervals are predicted based on the posterior
+    predictive distribution under this model.
+
+    Args:
+        b_0 (float): variance of the prior on the weights. Needs to be positive.
+        alpha_0 (float): prior location parameter for the observation noise. Needs to be positive.
+        delta_0 (float): prior scale parameter for the observation noise. Needs to be positive.
+        coverage (float): coverage of the confidence intervals, must be between 0 and 1.
+    """
+
+    def __init__(self, b_0: float, alpha_0: float, delta_0: float, coverage: float):
+        self.b_0 = b_0
+        self.alpha_0 = alpha_0
+        self.delta_0 = delta_0
+        self.coverage = coverage
+
+        self.bayesian_head = None
+
+    def fit_bayesian_head(self, X_train: np.ndarray, y_train: np.ndarray):
+        """Computes parameters for the posterior on the latent parameters given the training data.
+
+        The latent parameters for the Bayesian linear regression model are the weights
+        of the linear transformation and the observation noise. Features are pre-processed
+        by the forward method before being passed to the Bayesian model.
+
+        Args:
+            X_train (np.ndarray): training feature data, shape (n, d).
+            y_train (np.ndarray): training regression targets, shape (n, 1).
+        """
+        output_train = self.forward(X_train)
+        self.bayesian_head = BayesianLinearRegression(
+            b_0=self.b_0,
+            alpha_0=self.alpha_0,
+            delta_0=self.delta_0,
+            X_train=output_train,
+            y_train=y_train,
+        )
+
+    def output_to_prediction(self, output: np.ndarray) -> np.ndarray:
+        """Predicts confidence intervals based on the posterior predictive distribution.
+
+        Args:
+            output (np.ndarray): Processed feature data, shape (n, d').
+
+        Returns:
+            ndarray: confidence intervals [lower, upper], shape (n, 2).
+        """
+        if self.bayesian_head is None:
+            raise RuntimeError("Bayesian head not fitted on training set")
+        prediction = self.bayesian_head.predict_confidence_interval(
+            output, self.coverage
+        )
+        return prediction
+
+    def get_evidence(self) -> float:
+        """Returns a monotonic transformation of the evidence of the training data under
+        the Bayesian model. Used for model selection.
+
+        Returns:
+            float: evidence.
+        """
+        if self.bayesian_head is None:
+            raise RuntimeError("Bayesian head not fitted on training set")
+        return self.bayesian_head.evidence()
+
+    def forward(self, X: np.ndarray) -> np.ndarray:
+        raise NotImplementedError("Subclasses should implement this!")
+
+    def get_params(self) -> dict:
+        return {
+            "prediction_type": "bayesian",
+            "bayesian_coverage": self.coverage,
+            "weight_var": self.b_0,
+            "obs_noise_loc": self.alpha_0,
+            "obs_noise_scale": self.delta_0,
         }
