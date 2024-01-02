715029d2c224669c02c8f91cb09ff1d5908f5d22
Luca Rendsburg <luca.rendsburg@gmail.com>
2024-01-02 11:34:51 +0100

    experiment: 30 minutes / all models / data:simple

---
+++ run_experiments.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+python src/models/catboost/main_catboost_conformal.py --timeout 1800
+python src/models/catboost/main_catboost_quantile.py --timeout 1800
+python src/models/lightgbm/main_lightgbm_conformal.py --timeout 1800
+python src/models/lightgbm/main_lightgbm_quantile.py --timeout 1800
+python src/models/xgboost/main_xgboost_conformal.py --timeout 1800
+python src/models/xgboost/main_xgboost_quantile.py --timeout 1800
+python src/models/random_forest/main_rf_conformal.py --timeout 1800
+python src/models/random_forest/main_rf_quantile.py --timeout 1800
+python src/models/neural_networks/main_nn_bayesian.py --timeout 1800
+python src/models/neural_networks/main_nn_conformal.py --timeout 1800
+python src/models/neural_networks/main_nn_direct.py --timeout 1800
